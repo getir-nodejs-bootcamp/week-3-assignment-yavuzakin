0ba31efed67b792c2263084111bf483943284e69
yavuzakin <yavuzakin55@gmail.com>
2021-11-30 15:41:29 +0300

    posts route added

--- index.js
+++ index.js
@@ -2,11 +2,13 @@
 const express = require('express');
 const app = express();
 const userRoute = require('./routes/users');
+const postRoute = require('./routes/posts');
 
 // To be able to take json objects inside request body
 app.use(express.json());
 
 app.use('/users', userRoute);
+app.use('/posts', postRoute);
 
 app.listen(8000, () => {
     console.log('Listening port number 8000...');
